28519b55f727d11d10179ad266eef80d90f7e7ee
alexanderluo666 <alexanderluo0910@gmail.com>
2026-05-08 21:07:05 +0800

    better prototype

--- src/components/SpaceScene.tsx
+++ src/components/SpaceScene.tsx
@@ -1,35 +1,59 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import * as THREE from "three";
 
-function Planet() {
+type PlanetProps = {
+  position: [number, number, number];
+  size: number;
+  color: string;
+  speed: number;
+};
+
+function Planet({ position, size, color, speed }: PlanetProps) {
   const meshRef = useRef<THREE.Mesh>(null);
 
   useFrame(() => {
     if (!meshRef.current) return;
 
-    meshRef.current.rotation.y += 0.003;
+    meshRef.current.rotation.y += speed;
   });
 
   return (
-    <mesh ref={meshRef} position={[0, 0, 0]}>
-      <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial color="orange" />
+    <mesh ref={meshRef} position={position}>
+      <sphereGeometry args={[size, 32, 32]} />
+      <meshStandardMaterial color={color} />
     </mesh>
   );
 }
 
-
 export default function SpaceScene() {
   return (
-    <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
-      <ambientLight intensity={2} />
+    <Canvas camera={{ position: [0, 5, 12], fov: 75 }}>
+      <ambientLight intensity={1.5} />
 
-      <directionalLight position={[5, 5, 5]} intensity={2} />
+      <pointLight position={[10, 10, 10]} intensity={2} />
 
-      <Planet />
+      <Planet
+        position={[0, 0, 0]}
+        size={2}
+        color="orange"
+        speed={0.003}
+      />
+
+      <Planet
+        position={[5, 0, -3]}
+        size={1}
+        color="skyblue"
+        speed={0.01}
+      />
+
+      <Planet
+        position={[-6, 1, -5]}
+        size={1.5}
+        color="purple"
+        speed={0.005}
+      />
 
       <OrbitControls />
     </Canvas>
